68c08525c4a9608a66cdbd4043a63e88aef72b15
boenboenboen <150833855+boenboenboen@users.noreply.github.com>
2024-02-02 19:54:50 +0800

    Update

--- vgae/hg_dataloader.py
+++ vgae/hg_dataloader.py
@@ -29,22 +29,30 @@ class DataLoader():
         self.normalize_adj()
         self.to_pyt_sp()
         
+        
     def load_data(self,data):
         n = data['num_vertices']
         m = len(data['edge_list'])
+        
         edges = []
+        features = data['features']
         for i in range(m):
+            features_tensor = []
             for x in data['edge_list'][i]:
                 edges.append([x,i+n])
+                features_tensor.append(data['features'][x])
+            features_tensor = torch.stack(features_tensor)
+            mean_features = torch.mean(features_tensor, dim = 0)
+            features = torch.vstack((features, mean_features))
         edges = np.asarray(edges)
         row = edges.T[0]
         col = edges.T[1]
         adj_mat = sp.csr_matrix((np.ones_like(row), (row, col)), shape=(n+m, n+m))
         adj_mat = adj_mat + adj_mat.T
-        features = torch.eye(n+m)
         features = sp.coo_matrix(features.numpy())
         self.adj_orig = adj_mat
         self.features_orig = features
+        
     
     def mask_test_edges(self, val_frac, test_frac, no_mask):
         adj = self.adj_orig
